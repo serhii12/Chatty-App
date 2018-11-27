ecf387ea6feba08a7deb6648d2a379bab6a8de5f
Serhii Panchyshyn <panchyshyn.serhii@gmail.com>
2018-11-27 17:51:08 -0500

    finished websockets connection

--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
 const express = require('express');
-const SocketServer = require('ws').Server;
+const WebSocket = require('ws');
+
+const SocketServer = WebSocket.Server;
+const uuidv4 = require('uuid/v4');
 
 const PORT = 8081;
 const app = express();
@@ -13,8 +16,14 @@ const wss = new SocketServer({ server });
 wss.on('connection', ws => {
   console.log('Client connected');
   ws.on('message', data => {
-    console.log(data);
+    const msg = JSON.parse(data);
+    msg.id = uuidv4();
+    wss.clients.forEach(client => {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        console.log(JSON.stringify(msg));
+        client.send(JSON.stringify(msg));
+      }
+    });
   });
-  // Set up a callback for when a client closes the socket.
   ws.on('close', () => console.log('Client disconnected'));
 });
